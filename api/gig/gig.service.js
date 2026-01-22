063ed8830734dfa55ddddcd83f3a4a30263324d7
michalwie1 <michaliwieder@gmail.com>
2026-01-22 15:48:05 +0200

    Improve Filtering logic

--- api/gig/gig.service.js
+++ api/gig/gig.service.js
@@ -18,21 +18,40 @@ export const gigService = {
 }
 
 
-async function query(filterBy = {}) {
-    const criteria = _buildCriteria(filterBy)
-    const sort = _buildSort(filterBy)
+// async function query(filterBy = {}) {
+//     const criteria = _buildCriteria(filterBy)
+//     const sort = _buildSort(filterBy)
    
     
-    try {
-        const collection = await dbService.getCollection('gig')
-        const gigAgg = _getGigAggregation({ criteria, sort })
-        const gigs = await collection.aggregate(gigAgg).toArray()
-        return gigs
-    } catch (err) {
-        logger.error('cannot find gigs', err)
-        throw err
-    }
+//     try {
+//         const collection = await dbService.getCollection('gig')
+//         const gigAgg = _getGigAggregation({ criteria, sort })
+//         const gigs = await collection.aggregate(gigAgg).toArray()
+//         return gigs
+//     } catch (err) {
+//         logger.error('cannot find gigs', err)
+//         throw err
+//     }
+// }
+
+async function query(filterBy = {}) {
+  const { gigCriteria, sellerCriteria } = _buildCriteria(filterBy)
+  const sort = _buildSort(filterBy)
+
+  try {
+    const collection = await dbService.getCollection('gig')
+    const gigAgg = _getGigAggregation({
+      gigCriteria,
+      sellerCriteria,
+      sort
+    })
+    return await collection.aggregate(gigAgg).toArray()
+  } catch (err) {
+    logger.error('cannot find gigs', err)
+    throw err
+  }
 }
+
 
 
 async function getById(gigId) {
@@ -133,113 +152,220 @@ async function removeGigMsg(gigId, msgId) {
 	}
 }
 
-function _buildCriteria(filterBy) {
-  const criteria = {}
-  const { txt, maxBudget, loc, daysToMake, category, ownerLevel, ownerRate, avgResponseTime, sortField } = filterBy
+// function _buildCriteria(filterBy) {
+//   const criteria = {}
+//   const { txt, maxBudget, loc, daysToMake, category, ownerLevel, ownerRate, avgResponseTime, sortField } = filterBy
 
+//   if (txt) {
+//     criteria.$or = [
+//       { title: { $regex: txt, $options: 'i' } },
+//       { description: { $regex: txt, $options: 'i' } }
+//     ]
+//   }
+
+//   if (maxBudget) {
+//     criteria.price = { $lte: Number(maxBudget) }
+//   }
+
+//   if (daysToMake) {
+//     criteria.daysToMake = { $lte: Number(daysToMake) }
+//   }
+
+//   if (category) {
+//     criteria.tags = { $in: [category] }
+//   }
+
+//     if (avgResponseTime) {
+//     criteria.avgResponseTime = { $gte: Number(avgResponseTime) }
+//   }
+
+//   const choiceFilters = []
+
+//   if (ownerLevel?.length > 0) {
+//     const levels = [...ownerLevel]
+//     if (levels.includes(4)) levels.push(5)
+//     choiceFilters.push({ 'gig.owner.level': { $in: levels.map(Number) } })
+//   }
+
+//   if (ownerRate?.length > 0) {
+//     const rates = [...ownerRate]
+//     if (rates.includes(4)) rates.push(5)
+//     choiceFilters.push({ 'gig.owner.rate': { $in: rates.map(Number) } })
+//   }
+
+//   if (loc?.length > 0) {
+//     choiceFilters.push({ loc: { $in: loc } })
+//   }
+
+//   if (choiceFilters.length > 0) {
+//     if (criteria.$or) {
+//       criteria.$and = [
+//         { $or: criteria.$or }, 
+//         { $or: choiceFilters }  
+//       ]
+//       delete criteria.$or
+//     } else {
+//       criteria.$or = choiceFilters
+//     }
+//   }
+
+//   return criteria
+// }
+
+function _buildCriteria(filterBy) {
+  const gigCriteria = {}
+  const sellerOr = []
+
+  const {
+    txt,
+    maxBudget,
+    loc,
+    daysToMake,
+    category,
+    ownerLevel,
+    ownerRate,
+    avgResponseTime
+  } = filterBy
+
+  // ---- gig fields ----
   if (txt) {
-    criteria.$or = [
+    gigCriteria.$or = [
       { title: { $regex: txt, $options: 'i' } },
       { description: { $regex: txt, $options: 'i' } }
     ]
   }
 
-  if (maxBudget) {
-    criteria.price = { $lte: Number(maxBudget) }
+  if (maxBudget) gigCriteria.price = { $lte: +maxBudget }
+  if (daysToMake) gigCriteria.daysToMake = { $lte: +daysToMake }
+  if (category) gigCriteria.tags = { $in: [category] }
+  if (avgResponseTime) gigCriteria.avgResponseTime = { $gte: +avgResponseTime }
+
+  // ---- seller-details OR ----
+  if (loc?.length) {
+    sellerOr.push({ loc: { $in: loc } })
   }
 
-  if (daysToMake) {
-    criteria.daysToMake = { $lte: Number(daysToMake) }
-  }
-
-  if (category) {
-    criteria.tags = { $in: [category] }
-  }
-
-    if (avgResponseTime) {
-    criteria.avgResponseTime = { $gte: Number(avgResponseTime) }
-  }
-
-  const choiceFilters = []
-
-  if (ownerLevel?.length > 0) {
+  if (ownerLevel?.length) {
     const levels = [...ownerLevel]
     if (levels.includes(4)) levels.push(5)
-    choiceFilters.push({ 'owner.level': { $in: levels.map(Number) } })
+    sellerOr.push({ 'owner.level': { $in: levels.map(Number) } })
   }
 
-  if (ownerRate?.length > 0) {
+  if (ownerRate?.length) {
     const rates = [...ownerRate]
     if (rates.includes(4)) rates.push(5)
-    choiceFilters.push({ 'owner.rate': { $in: rates.map(Number) } })
+    sellerOr.push({ 'owner.rate': { $in: rates.map(Number) } })
   }
 
-  if (loc?.length > 0) {
-    choiceFilters.push({ loc: { $in: loc } })
+  return {
+    gigCriteria,
+    sellerCriteria: sellerOr.length ? { $or: sellerOr } : null
   }
-
-  if (choiceFilters.length > 0) {
-    if (criteria.$or) {
-      criteria.$and = [
-        { $or: criteria.$or }, 
-        { $or: choiceFilters }  
-      ]
-      delete criteria.$or
-    } else {
-      criteria.$or = choiceFilters
-    }
-  }
-
-  return criteria
 }
+
+
 
 
 
 function _buildSort(filterBy) {
   const { sortField } = filterBy
 
-  if (sortField === 'lowest') return { price: 1 }       
+  if (sortField === 'lowest price') return { price: 1 }       
   if (sortField === 'newest') return { _id: -1 } 
 
   return { title: 1 }                
 }
 
-function _getGigAggregation({ criteria = {}, sort = {title: 1} } = {}) {
-    const gigAggregate = [
-        { $match: criteria },
-        { $sort: sort },
-        {
-            $lookup: {
-                from: 'owner',
-                localField: 'ownerId',
-                foreignField: '_id',
-                as: 'owner'
-            }
-        },
-        { $unwind: '$owner' },
-        {
-            $project: {
-                title: 1,
-                price: 1,
-                daysToMake: 1,
-                description: 1,
-                avgResponseTime: 1,
-                loc: 1,
-                imgUrls: 1,
-                tags: 1,
-                categories: 1,
-                owner: {
-                    _id: '$owner._id',
-                    fullname: '$owner.fullname',
-                    imgUrl: '$owner.imgUrl',
-                    level: '$owner.level',
-                    rate: '$owner.rate'
-                }
-            }
-        }
+// function _getGigAggregation({ criteria = {}, sort = {title: 1} } = {}) {
+//     const gigAggregate = [
+//         { $match: criteria },
+//         { $sort: sort },
+//         {
+//             $lookup: {
+//                 from: 'owner',
+//                 localField: 'ownerId',
+//                 foreignField: '_id',
+//                 as: 'owner'
+//             }
+//         },
+//         { $unwind: '$owner' },
+//         {
+//             $project: {
+//                 title: 1,
+//                 price: 1,
+//                 daysToMake: 1,
+//                 description: 1,
+//                 avgResponseTime: 1,
+//                 loc: 1,
+//                 imgUrls: 1,
+//                 tags: 1,
+//                 categories: 1,
+//                 owner: {
+//                     _id: '$owner._id',
+//                     fullname: '$owner.fullname',
+//                     imgUrl: '$owner.imgUrl',
+//                     level: '$owner.level',
+//                     rate: '$owner.rate'
+//                 }
+//             }
+//         }
 		
-    ]
+//     ]
 
-	return(gigAggregate)
+// 	return(gigAggregate)
+// }
+
+function _getGigAggregation({
+  gigCriteria = {},
+  sellerCriteria = null,
+  sort = { title: 1 }
+} = {}) {
+
+  const pipeline = [
+    { $match: gigCriteria },
+
+    {
+      $lookup: {
+        from: 'owner',
+        localField: 'ownerId',
+        foreignField: '_id',
+        as: 'owner'
+      }
+    },
+    { $unwind: '$owner' }
+  ]
+
+  // 👇 seller details MUST be here
+  if (sellerCriteria) {
+    pipeline.push({ $match: sellerCriteria })
+  }
+
+  pipeline.push(
+    { $sort: sort },
+    {
+      $project: {
+        title: 1,
+        price: 1,
+        daysToMake: 1,
+        description: 1,
+        avgResponseTime: 1,
+        loc: 1,
+        imgUrls: 1,
+        tags: 1,
+        categories: 1,
+        owner: {
+          _id: '$owner._id',
+          fullname: '$owner.fullname',
+          imgUrl: '$owner.imgUrl',
+          level: '$owner.level',
+          rate: '$owner.rate'
+        }
+      }
+    }
+  )
+
+  return pipeline
 }
+
+
 
